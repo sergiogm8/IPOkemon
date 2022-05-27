7029dc734926a2f827de965b9676888d2a7ea940
Sergio Garrido <sergarridom8@gmail.com>
2022-05-27 16:47:29 +0200

    Clase Pokemon

---
+++ IPOkemon/IPOkemon/Pokemon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPOkemon
+{
+    public class Pokemon
+    {
+        public string nombre { get; set; }
+        public int nivel { get; set; }
+        public double exp { get; set; }
+        public string tipo { get; set; }
+        public bool capturado { get; set; }
+        public string descripcion { get; set; }
+        public Uri sprite { get; set; }
+
+        public Pokemon(string nombre, int nivel, double exp, string tipo, bool capturado, string descripcion, Uri sprite)
+        {
+            this.nombre = nombre;
+            this.nivel = nivel;
+            this.exp = exp;
+            this.tipo = tipo;
+            this.capturado = capturado;
+            this.descripcion = descripcion;
+            this.sprite = sprite;
+        }
+    }
+}

05cd98a04739e1aef8d3aa419b829b524ac7f92e
ElSergyz <58768971+ElSergyz@users.noreply.github.com>
2022-05-30 12:28:39 +0200

    Ajustes hechos

--- IPOkemon/IPOkemon/PokedexPage.xaml.cs
+++ IPOkemon/IPOkemon/PokedexPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -44,6 +45,7 @@ namespace IPOkemon
 
             pokemons.Add(azumarill);
             pokemons.Add(articuno);
+
         }
 
         private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
